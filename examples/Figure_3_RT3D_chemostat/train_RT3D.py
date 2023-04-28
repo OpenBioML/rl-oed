d4dc453cdd756220a19583f8407ccbcba62e49ce
Neythen Treloar <neythen.t@gmail.com>
2023-04-28 07:39:42 +0100

    removed debugging code

--- examples/Figure_3_RT3D_chemostat/train_RT3D.py
+++ examples/Figure_3_RT3D_chemostat/train_RT3D.py
@@ -55,7 +55,7 @@ def train_RT3D(cfg: DictConfig):
     update_count = 0
 
     ### training loop
-    for episode in range(10):
+    for episode in range(0, cfg.environment.N_control_intervals):
         actual_params = np.random.uniform(
             low=cfg.environment.actual_params,
             high=cfg.environment.actual_params,
